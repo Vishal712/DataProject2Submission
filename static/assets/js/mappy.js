87823f5fd1f4920135142cb19c91640eaad079a9
Vishal Patel <vishalpatel@Vishals-MacBook-Pro.local>
2020-10-16 18:11:36 -0700

    Got map done

--- static/assets/js/mappy.js
+++ static/assets/js/mappy.js
@@ -1,12 +1,11 @@
-// Creating our initial map object
-// We set the longitude, latitude, and the starting zoom level
-// This gets inserted into the div with an id of 'map'
+// Creating map object
 var myMap = L.map("map", {
-  center: [37.09, -95.71],
+  center: [38.5, -97.6],
+  zoomSnap: 0.25,
   zoom: 5
 });
-// Adding a tile layer (the background map image) to our map
-// We use the addTo method to add objects to our map
+
+// Adding tile layer to the map
 L.tileLayer("https://api.mapbox.com/styles/v1/hyrum78/ckg9tsvf803p519pet1f9cbmk/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaHlydW03OCIsImEiOiJja2c1bGdpZjQwdWIwMnJtc2dscmd0eHFzIn0.lEHm3LRxyN6_azwPjzIf5A", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
@@ -14,20 +13,50 @@ L.tileLayer("https://api.mapbox.com/styles/v1/hyrum78/ckg9tsvf803p519pet1f9cbmk/
   zoomOffset: -1,
   id: "mapbox/streets-v11",
   accessToken: API_KEY
-}).addTo(myMap);
+  }).addTo(myMap);
 
+  // pull icon
+  var ballIcon = L.icon({
+    iconUrl: '../static/images/ballicon.png',
+    iconSize: [30,30],
+    popupAnchor: [-10, -30],
+  }); 
 
 d3.json("/NBALocation", function(data) {
-  console.log(data)
-  var markers = L.markerClusterGroup();
-  for (i = 0; i < data.length; i++) {
-    var location = [data[i].lat, data[i].lng]
-    console.log(data)
-    
+  d3.json("/Averages", function(averageData){
+
+  // Create a new marker cluster group
+  var markers = L.markerClusterGroup(
+
+  );
+  // Loop through data
+  for (var i = 0; i < data.length; i++) {
+
+    // Set the data location property to a variable
+    var location = [data[i].lat, data[i].lng];
+
+    // Check for location property
     if (location) {
-      markers.addLayer(L.marker([location[0], location[1]])
-        .bindPopup(data[i].school));
+      var filteredAvg = averageData.filter(playername => playername.school == data[i].school)[0];
+      
+      //there are a couple of players with no listed school, so if there data is undefined, it will count that player's average
+      if(filteredAvg == null){
+        filteredAvg = data[i]
+      }
+      // Add a new marker to the cluster group and bind a pop-up
+      markers.addLayer(L.marker([location[0], location[1]],{icon: ballIcon})
+        .bindPopup("School: " + data[i].school + "<br>" +
+                    "Avg NBA Player Stats:" + "<br>" + 
+                    "PPG " + filteredAvg.PPG + "<br>" + 
+                    "APG " + filteredAvg.APG + "<br>" + 
+                    "RPG " + filteredAvg.RPG));
     }
+
   }
-myMap.addLayer(markers)
-});
+
+  // Add our marker cluster layer to the map
+  myMap.addLayer(markers);
+  
+ 
+})
+})
